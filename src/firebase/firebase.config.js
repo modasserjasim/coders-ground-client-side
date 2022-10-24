5615b9b2168d7503ec40354d8593d8e406633378
Modasserul Hoque <modasserulhaq.jasim@gmail.com>
2022-10-25 03:36:45 +0600

    Project started

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDCLZC8TRXavjE9bac5gU8xKRAvfuyse1I",
+    authDomain: "codersground-38a94.firebaseapp.com",
+    projectId: "codersground-38a94",
+    storageBucket: "codersground-38a94.appspot.com",
+    messagingSenderId: "374130404523",
+    appId: "1:374130404523:web:beebdb253e7b00688bdbee"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
